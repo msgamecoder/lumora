5bf07a4ae56889b31e5296cc6bcedb3a220f701d
msgamecoder <olamilekandamilaraa@gmail.com>
2025-06-21 23:06:54 +0100

    lumora

--- mekautils/mekasendMail.js
+++ mekautils/mekasendMail.js
@@ -19,13 +19,12 @@ const sendVerificationEmail = async (to, url, username, world) => {
         <p>Hi <b>@${username}</b>,</p>
         <p>Welcome to Lumora (${world === 'one' ? 'World One 🌍' : 'World Two 🌎'})</p>
         <p>Click below to verify your account:</p>
-        <p><a href="${url}" style="padding: 10px 16px; background: #00ffe0; color: #000; text-decoration: none; border-radius: 5px;">Verify Account</a></p>
+        <p><a href="${url}" style="padding: 10px 16px; background: #fff; color: #000; text-decoration: none; border-radius: 5px;">Verify Account</a></p>
         <p>If you didn’t register, ignore this email.</p>
         <p style="font-size: 0.8rem; color: #777;">🔗 Lumora | MΞKΛ Core v4</p>
       </div>
     `
   };
-
   await transporter.sendMail(mailOptions);
 };
 
