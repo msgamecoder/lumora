477ead96ea61e65595b066c9c4457272931849f6
msgamecoder <olamilekandamilaraa@gmail.com>
2025-06-22 18:17:19 +0100

    lumora

--- mekaroutes/mekaauth.js
+++ mekaroutes/mekaauth.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const { registerUser, verifyUser, recoverUnverifiedWithPassword } = require('../mekacontrollers/mekaauthController');
 const { loginUser } = require('../mekacontrollers/mekalogin');
 const { checkUsername, checkEmail, checkPhone } = require('../mekacontrollers/mekaauthCheck');
+const forgotController = require('../mekacontrollers/mekaforgotController');
 
 router.post('/meka/register', registerUser);
 router.get('/verify/:token', verifyUser);
@@ -12,4 +13,7 @@ router.post('/meka/check-username', checkUsername);
 router.post('/meka/check-email', checkEmail);
 router.post('/meka/check-phone', checkPhone);
 router.post('/meka/recover', recoverUnverifiedWithPassword);
+// Forgot password routes
+router.post('/forgot', forgotController.sendResetLink);
+router.post('/reset', forgotController.resetPassword);
 module.exports = router;
