2f6dffb4a4530acbb283dbcc5153cc238414a620
msgamecoder <olamilekandamilaraa@gmail.com>
2025-07-21 03:44:42 -0700

    lumora

--- mekaroutes/mekaauth.js
+++ mekaroutes/mekaauth.js
@@ -1,12 +1,23 @@
 // mekaroutes/auth.js
 const express = require('express');
 const router = express.Router();
-const { registerUser, verifyUser, recoverUnverifiedWithPassword } = require('../mekacontrollers/mekaauthController');
+const {
+  registerUser,
+  verifyUser,
+  recoverUnverifiedWithPassword
+} = require('../mekacontrollers/mekaauthController');
 const { loginUser } = require('../mekacontrollers/mekalogin');
-const { checkUsername, checkEmail, checkPhone } = require('../mekacontrollers/mekaauthCheck');
+const {
+  checkUsername,
+  checkEmail,
+  checkPhone
+} = require('../mekacontrollers/mekaauthCheck');
 const forgotController = require('../mekacontrollers/mekaforgotController');
 const { checkTokenValidity } = require('../mekacontrollers/mekacheckToken');
-const { banOnReviewLogout } = require('../mekacontrollers/mekaban');
+const {
+  banOnReviewLogout,
+  getReviewStartTime // ✅ add this line
+} = require('../mekacontrollers/mekaban');
 
 router.post('/meka/register', registerUser);
 router.post('/meka/verify', verifyUser);
@@ -15,9 +26,15 @@ router.post('/meka/check-username', checkUsername);
 router.post('/meka/check-email', checkEmail);
 router.post('/meka/check-phone', checkPhone);
 router.post('/meka/recover', recoverUnverifiedWithPassword);
+
 // Forgot password routes
 router.post('/meka/forgot', forgotController.sendResetCode);
 router.post('/meka/reset', forgotController.resetPassword);
 router.post('/meka/check-token', checkTokenValidity);
+
 router.post("/meka/ban-on-review-logout", banOnReviewLogout);
+
+// ✅ NEW: Add the GET route here
+router.get("/meka/review-start-time", getReviewStartTime);
+
 module.exports = router;
