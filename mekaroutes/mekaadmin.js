6f22a2886065d2afed376aacc69c81b87c17aca0
msgamecoder <olamilekandamilaraa@gmail.com>
2025-07-21 13:38:28 -0700

    lumora

--- mekaroutes/mekaadmin.js
+++ mekaroutes/mekaadmin.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getFlaggedUsers, unflagUser, banUser, sendReviewMessage } = require('../mekacontrollers/mekaadminController');
+const { getFlaggedUsers, unflagUser, banUser, sendReviewMessage, getReviewMessage } = require('../mekacontrollers/mekaadminController');
 
 // Get all flagged accounts
 router.get('/flagged', getFlaggedUsers);
