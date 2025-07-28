e2389c391ab7ee856cbbc008e12c2998939c1b27
msgamecoder <olamilekandamilaraa@gmail.com>
2025-07-28 06:28:00 -0700

    lumora

--- mekaconfig/mekafirebase.js
+++ mekaconfig/mekafirebase.js
@@ -2,7 +2,7 @@ const admin = require('firebase-admin');
 const path = require('path');
 
 // Absolute path to the service account key
-const serviceAccountPath = path.join(__dirname, 'lumorapush-firebase-adminsdk-fbsvc-3a9ce4a410.json');
+const serviceAccountPath = path.join(__dirname, 'lumorapush-firebase-adminsdk-fbsvc-0f8abc6e29.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccountPath)
