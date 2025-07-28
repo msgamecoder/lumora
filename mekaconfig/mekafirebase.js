6695a5114c437e6d785430e7f8e72ea2f2ab5508
msgamecoder <olamilekandamilaraa@gmail.com>
2025-07-28 06:53:29 -0700

    Update mekafirebase.js

--- mekaconfig/mekafirebase.js
+++ mekaconfig/mekafirebase.js
@@ -1,11 +1,16 @@
 const admin = require('firebase-admin');
-const path = require('path');
-
-// Absolute path to the service account key
-const serviceAccountPath = path.join(__dirname, 'lumorapush-firebase-adminsdk-fbsvc-0f8abc6e29.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccountPath)
+  credential: admin.credential.cert({
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
+  }),
 });
-
-module.exports = admin;
