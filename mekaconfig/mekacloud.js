1038714b3145c0ac3d22ce2c745a47ce0dd26f89
msgamecoder <olamilekandamilaraa@gmail.com>
2025-07-29 08:43:31 -0700

    lumora

---
+++ mekaconfig/mekacloud.js
@@ -0,0 +1,10 @@
+// mekaconfig/mekacloud.js
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+  cloud_name: 'dlkiew9d3',
+  api_key: '528498392385416',
+  api_secret: 'oKosddorVXFvD65oiuNMJwkAZ0U',
+});
+
+export default cloudinary;
